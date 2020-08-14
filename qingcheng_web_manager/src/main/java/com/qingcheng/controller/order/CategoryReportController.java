8a243de1de27065946a1f4a28edbd514b8150c35
Coderxf <gl853211@gmail.com>
2020-08-14 23:53:28 +0800

    qingcheng项目

---
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/order/CategoryReportController.java
@@ -0,0 +1,33 @@
+package com.qingcheng.controller.order;
+
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.qingcheng.pojo.order.CategoryReport;
+import com.qingcheng.service.order.CategoryReportService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categoryReport")
+public class CategoryReportController {
+
+
+    @Reference
+    private CategoryReportService categoryReportService;
+
+
+    /**
+     * 昨天的数据统计(测试)
+     * @return
+     */
+    @GetMapping("/yesterday")
+    public List<CategoryReport> yesterday(){
+        // 得到昨天的日期
+        LocalDate localDate = LocalDate.now().minusDays(1);
+        return categoryReportService.categoryReport(localDate);
+    }
+}
